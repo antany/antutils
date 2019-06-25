edd7461af2eb499455514ab782c38a71ff7c23a4
antany <antany84@gmail.com>
2019-06-25 00:17:14 -0400

    Added IO close method

---
+++ src/main/java/ca/antany/common/io/IOUtils.java
@@ -0,0 +1,19 @@
+package ca.antany.common.io;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class IOUtils {
+
+	private static Logger logger = Logger.getLogger(IOUtils.class.getName());
+	
+	public static void close(Closeable object) {
+		try {
+			object.close();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE,e,null);
+		}
+	}
+}
